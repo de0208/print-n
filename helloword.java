c14a621a7a8c03b3fee3f4003900f32f19831dfc
de0208 <47915407+de0208@users.noreply.github.com>
2019-03-06 10:25:20 -0500

    Create helloword.java

---
+++ helloword.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class HelloWord
+{
+     public static void main(String []args)
+     {
+		
+		System.out.println("Hello Word N!");
+     }
+}
